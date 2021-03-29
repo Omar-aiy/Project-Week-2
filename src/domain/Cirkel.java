1ef602dcd4160feee1341bd332e760359a172294
Omar <omar.aiyyachi@gmail.com>
2021-03-29 11:24:09 +0200

    Initial commit

---
+++ src/domain/Cirkel.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Cirkel {
+    private Punt middelPunt;
+    private int radius;
+
+    public Cirkel(Punt middelpunt, int straal) {
+        if (middelpunt == null) throw new IllegalArgumentException();
+        if (straal <= 0) throw new IllegalArgumentException();
+        if (straal > Math.min(middelpunt.getX(), middelpunt.getY())) throw new IllegalArgumentException();
+        this.middelPunt = middelpunt;
+        this.radius = straal;
+    }
+
+    public Punt getMiddelPunt() {
+        return middelPunt;
+    }
+
+    public void setMiddelPunt(Punt middelPunt) {
+        this.middelPunt = middelPunt;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cirkel cirkel = (Cirkel) o;
+        return Double.compare(cirkel.radius, radius) == 0 && Objects.equals(middelPunt, cirkel.middelPunt);
+    }
+
+    @Override
+    public String toString() {
+        return "Cirkel met middelpunt " + getMiddelPunt().toString() + " en straal " + getRadius();
+    }
+}
---
+++ src/domain/DomainException.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class DomainException extends Exception {
+
+    public DomainException() {
+    }
+
+    public DomainException(String message) {
+        super(message);
+    }
+}
