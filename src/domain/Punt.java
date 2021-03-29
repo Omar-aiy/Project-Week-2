9bb838046f1a5f8df23db39cb19c02077888ad48
SiebeCooreman <79042074+SiebeCooreman@users.noreply.github.com>
2021-03-29 11:32:26 +0200

    siebe-alex_story02

--- src/domain/Punt.java
+++ src/domain/Punt.java
@@ -1,6 +1,31 @@
 package domain;
-
 public class Punt {
-    trlalalala
-}
+    private int x;
+    private int y;
 
+    public Punt(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean equals(Punt a){
+        if (a == null){
+            return false;
+        } else {
+            return a.getX() == this.x && a.getY() == this.y;
+        }
+    }
+    public String toString(){
+        String result = "";
+        result += "(" + this.getX() + ", " + this.getY() + ")";
+        return result;
+    }
+}
